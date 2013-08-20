f43dfd94e873ee3b164f1f0e81c2bae7f433748f
Chris <ulrich@tcs-germany.de>
2013-08-20 14:02:05 +0200

    fixed css, fixed task adding

--- apps/promet/js/framework.js
+++ apps/promet/js/framework.js
@@ -37,7 +37,7 @@ document.onreadystatechange = function() {
   else if (
       navigator.userAgent.match(/iPad/i)
       ){ appendSheet("themes/apple/theme.css"); }
-  else { appendSheet("themes/android/theme.css"); }
+  else { appendSheet("themes/windowsphone/theme.css"); }
   }
   function hideAddressBar(){
     if(document.documentElement.scrollHeight<window.innerHeight/window.devicePixelRatio)
@@ -88,7 +88,10 @@ document.onreadystatechange = function() {
                 hideLoading();
                 var ob = mainDiv.getElementsByTagName("script");
                 for(var i=0; i<ob.length; i++){
-                  if(ob[i].text!=null) eval(ob[i].text);
+                  if(ob[i].text!=null){
+                    var ascript = ob[i].text;
+                    eval(ascript);
+                  }
                 }
               }
             else
