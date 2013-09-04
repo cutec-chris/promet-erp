db10931426a2056ce57be3a39fff7ab67b4dbebb
Chris <chris@minimac.(none)>
2013-09-04 18:04:11 +0200

    osx setup

--- promet/setup/darwin/install.sh
+++ promet/setup/darwin/install.sh
@@ -9,8 +9,10 @@ export DC_APP_DIR=$1/prometerp.app
 export DC_INSTALL_DIR=$DC_APP_DIR/Contents/MacOS
 
 mkdir -p $DC_INSTALL_DIR
+mkdir -p $DC_INSTALL_DIR/tools
+mkdir -p $DC_APP_DIR/Contents/Resources
 
-mkdir -p $DC_INSTALL_DIR/plugins
+cp ../../resources/world.icns $DC_APP_DIR/Contents/Resources
 
 # directories
 # Copy files
