21f8c5b04dd1a88b524a457ba99adf283cf11b9c
Chris <chris@minimac.(none)>
2013-09-07 08:13:32 +0200

    osx setup

--- promet/setup/upload_osx.sh
+++ promet/setup/upload_osx.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-Version=$(sed 's/\r//g' ../../source/base/version.inc).$(sed 's/\r//g' ../../source/base/revision.inc)
+Version=$(sed 's/\r//g' ../source/base/version.inc).$(sed 's/\r//g' ../source/base/revision.inc)
 Version=$(echo $Version | sed 's/\n//g');
 
-echo $Version
-
 chmod 644 output/*
-scp output/*_$(echo $Version)_*.dmg autoupload@ullihome.de:promet_upload_target
+scp output/*$Version_*.dmg autoupload@ullihome.de:promet_upload_target
