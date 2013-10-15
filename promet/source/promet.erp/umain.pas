0b345855ddff76a37ea179bf8b698201b10d3047
Chris <ulrich@tcs-germany.de>
2013-10-15 10:09:53 +0200

    load financial asynchron

--- promet/source/promet.erp/umain.pas
+++ promet/source/promet.erp/umain.pas
@@ -779,51 +779,6 @@ begin
       aDataSet.Destroy;
       Data.RegisterLinkHandler('INVENTORY',@fMainTreeFrame.OpenLink);
     end;
-  //Financial
-  {$region}
-  if (Data.Users.Rights.Right('BANKACCNTS') > RIGHT_NONE)
-  or (Data.Users.Rights.Right('SALESLIST') > RIGHT_NONE) then
-    begin
-      NewNode;
-      Node.Height := 34;
-      TTreeEntry(Node.Data).Typ := etFinancial;
-      if Data.Users.Rights.Right('BANKACCNTS') > RIGHT_NONE then
-        begin
-          Data.RegisterLinkHandler('ACCOUNTEXCHANGE',@fMainTreeFrame.OpenLink);
-          NewNode1;
-          TTreeEntry(Node1.Data).Typ := etBanking;
-          NewNode2;
-          TTreeEntry(Node2.Data).Typ := etAccounts;
-          NewNode3;
-          TTreeEntry(Node3.Data).Typ := etNewAccount;
-          Accounts := TAccounts.Create(nil,Data,aConn);
-          Accounts.CreateTable;
-          Accounts.Open;
-          Accounts.DataSet.First;
-          while not Accounts.DataSet.EOF do
-            begin
-              NewNode3;
-              TTreeEntry(Node3.Data).Rec := Accounts.GetBookmark;
-              TTreeEntry(Node3.Data).Text[0] := Accounts.FieldByName('NAME').AsString;
-              TTreeEntry(Node3.Data).Typ := etAccount;
-              Accounts.DataSet.Next;
-            end;
-          Accounts.Free;
-          NewNode2;
-          TTreeEntry(Node2.Data).Typ := etNewTransfer;
-          NewNode2;
-          TTreeEntry(Node2.Data).Typ := etAccountingQue;
-        end;
-      if Data.Users.Rights.Right('SALESLIST') > RIGHT_NONE then
-        begin
-          fMain.pcPages.AddTabClass(TfFilter,strSalesList,@fMain.AddSalesList,-1,True);
-          NewNode1;
-          TTreeEntry(Node1.Data).Typ := etSalesList;
-          TTreeEntry(Node1.Data).Action := fMain.acSalesList;
-        end;
-    end;
-  {$endregion}
-  //debugln('Inventory/Financial: '+IntToStr(GetTickCount64-aTime));
   //Add Statistics
   {$ifndef heaptrc}
   Synchronize(@AddStatistics);
@@ -1015,10 +970,19 @@ begin
             NewNode;
             TTreeEntry(Node.Data).Typ := etMeetings;
           end;
+        //Inventory
         if (Data.Users.Rights.Right('INVENTORY') > RIGHT_NONE) then
           begin
             NewNode;
             TTreeEntry(Node.Data).Typ := etInventory;
+          end;
+        //Financial
+        if (Data.Users.Rights.Right('BANKACCNTS') > RIGHT_NONE)
+        or (Data.Users.Rights.Right('SALESLIST') > RIGHT_NONE) then
+          begin
+            NewNode;
+            Node.Height := 34;
+            TTreeEntry(Node.Data).Typ := etFinancial;
           end;
 
         //bStart := TStarterThread.Create;
@@ -2862,6 +2826,43 @@ begin
       etMeetings:
         begin
           umeetingframe.AddToMainTree(fMain.acNewMeeting,Node);
+        end;
+      etFinancial:
+        begin
+          if Data.Users.Rights.Right('BANKACCNTS') > RIGHT_NONE then
+            begin
+              Data.RegisterLinkHandler('ACCOUNTEXCHANGE',@fMainTreeFrame.OpenLink);
+              Node1 := fMainTreeFrame.tvMain.Items.AddChildObject(Node,'',TTreeEntry.Create);
+              TTreeEntry(Node1.Data).Typ := etBanking;
+              Node2 := fMainTreeFrame.tvMain.Items.AddChildObject(Node1,'',TTreeEntry.Create);
+              TTreeEntry(Node2.Data).Typ := etAccounts;
+              Node3 := fMainTreeFrame.tvMain.Items.AddChildObject(Node2,'',TTreeEntry.Create);
+              TTreeEntry(Node3.Data).Typ := etNewAccount;
+              Accounts := TAccounts.Create(nil,Data,aConn);
+              Accounts.CreateTable;
+              Accounts.Open;
+              Accounts.DataSet.First;
+              while not Accounts.DataSet.EOF do
+                begin
+                  Node3 := fMainTreeFrame.tvMain.Items.AddChildObject(Node2,'',TTreeEntry.Create);
+                  TTreeEntry(Node3.Data).Rec := Accounts.GetBookmark;
+                  TTreeEntry(Node3.Data).Text[0] := Accounts.FieldByName('NAME').AsString;
+                  TTreeEntry(Node3.Data).Typ := etAccount;
+                  Accounts.DataSet.Next;
+                end;
+              Accounts.Free;
+              Node2 := fMainTreeFrame.tvMain.Items.AddChildObject(Node1,'',TTreeEntry.Create);
+              TTreeEntry(Node2.Data).Typ := etNewTransfer;
+              Node2 := fMainTreeFrame.tvMain.Items.AddChildObject(Node1,'',TTreeEntry.Create);
+              TTreeEntry(Node2.Data).Typ := etAccountingQue;
+            end;
+          if Data.Users.Rights.Right('SALESLIST') > RIGHT_NONE then
+            begin
+              fMain.pcPages.AddTabClass(TfFilter,strSalesList,@fMain.AddSalesList,-1,True);
+              Node1 := fMainTreeFrame.tvMain.Items.AddChildObject(Node,'',TTreeEntry.Create);
+              TTreeEntry(Node1.Data).Typ := etSalesList;
+              TTreeEntry(Node1.Data).Action := fMain.acSalesList;
+            end;
         end;
       end;
     end;
