05b340f615a1bb4d7a1ea54d04227b66e3b77186
Chris <info@cu-tec.de>
2014-01-19 17:15:46 +0100

    Setup:add receivers,senders to service package

--- promet/setup/i386-linux/copy_to_builddir_web.sh
+++ promet/setup/i386-linux/copy_to_builddir_web.sh
@@ -10,6 +10,8 @@ cp ../../output/$1-linux/web/mta $2/tools
 cp ../../output/$1-linux/web/nntpserver $2/tools
 cp ../../output/$1-linux/web/svnserver $2/tools
 cp ../../output/$1-linux/web/httpserver $2/tools
+install ../../output/$1-linux/tools/*receiver $2/tools
+install ../../output/$1-linux/tools/*sender $2/tools
 Version=$(sed 's/\r//g' ../../source/base/version.inc).$(sed 's/\r//g' ../../source/base/revision.inc)
 Version=$(echo $Version | sed 's/\n//g');
 cp $2/tools/local_appbase ../executables/$Version/$1
@@ -20,3 +22,5 @@ cp $2/tools/svnserver ../executables/$Version/$1
 cp $2/tools/processmanager ../executables/$Version/$1
 cp $2/tools/promet-process ../executables/$Version/$1
 cp $2/tools/cmdwizardmandant ../executables/$Version/$1
+cp $2/tools/*receiver ../executables/$Version/$1
+cp $2/tools/*sender ../executables/$Version/$1
