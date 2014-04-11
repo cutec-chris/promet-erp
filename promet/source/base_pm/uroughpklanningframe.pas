83ab59e206e9869a37425af19257682b64cfc39c
Chris <ulrich@tcs-germany.de>
2014-04-11 16:29:05 +0200

    Roughplanning:goto Project

--- promet/source/base_pm/uroughpklanningframe.pas
+++ promet/source/base_pm/uroughpklanningframe.pas
@@ -455,7 +455,7 @@ end;
 procedure TfRoughPlanningFrame.bDelegated2Click(Sender: TObject);
 begin
   if Assigned(FRough.Tree.Objects[0,FRough.Tree.Row]) then
-    Data.GotoLink('PROJECTS.ID@'+IntToStr(TInterval(FRough.Tree.Objects[0,FRough.Tree.Row]).Id));
+    Data.GotoLink('PROJECTS@'+IntToStr(TInterval(FRough.Tree.Objects[0,FRough.Tree.Row]).Id));
 end;
 procedure TfRoughPlanningFrame.acUseExecute(Sender: TObject);
 begin
