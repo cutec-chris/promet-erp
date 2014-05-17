fed6b5f72e36da460279de4114f49b39145df57a
Chris <info@cu-tec.de>
2014-05-17 17:48:58 +0200

    Translations:startet english translation

--- promet/setup/build_portable.sh
+++ promet/setup/build_portable.sh
@@ -3,6 +3,6 @@ Version=$(sed 's/\r//g' ../source/base/version.inc).$(sed 's/\r//g' ../source/ba
 Version=$(echo $Version | sed 's/\n//g');
 cd portableapps
 sh build_all.sh
-#sh upload_portable.sh
+sh upload_portable.sh
 sh change_wiki.sh
 cd ..
