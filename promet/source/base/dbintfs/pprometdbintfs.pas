b6e406d1154cef8f93b1d0a62edae0b5947ceffd
Chris <info@cu-tec.de>
2014-07-27 22:21:50 +0200

    move zugferd to base dbintfs

--- promet/source/base/dbintfs/pprometdbintfs.pas
+++ promet/source/base/dbintfs/pprometdbintfs.pas
@@ -14,7 +14,7 @@ uses
   ubaseapplicationtools, uWiki, uEncrypt, usync, uSessionDBClasses, utask, 
   uvtools, uimpvcal, uDocumentProcess, uProcessManager, uProcessManagement, 
   uprometipc, umeeting, uBaseDocPages, FPReadJPEGintfd, uthumbnails, 
-  fpthumbresize, ucalc, uimport, uPasswordSave, LazarusPackageIntf;
+  fpthumbresize, ucalc, uimport, uPasswordSave, uzugferd, LazarusPackageIntf;
 
 implementation
 
