f16e287ab46eb20b2dc49fec864930bdd7d6b792
Andreas Rechlin <rechlin@tcs-germany.de>
2014-08-04 08:30:03 +0200

    Repair:added repairimages

---
+++ promet/source/base_repair/urepairimages.pas
@@ -0,0 +1,26 @@
+unit urepairimages;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfRepairImages = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  fRepairImages: TfRepairImages;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
