c8c85ac40954ca7c1f415b53570092c905db5724
Chris <info@cu-tec.de>
2014-09-18 17:42:06 +0200

    new Version

--- general/general_nogui.pas
+++ general/general_nogui.pas
@@ -8,7 +8,7 @@ interface
 
 uses
   Utils, uGeneralStrConsts, SecureUtils, umashineid, uModifiedDS, uRTFtoTXT, 
-  MathParser, Aspell, usimpleprocess, LazarusPackageIntf;
+  MathParser, Aspell, usimpleprocess, uSunRise, LazarusPackageIntf;
 
 implementation
 
