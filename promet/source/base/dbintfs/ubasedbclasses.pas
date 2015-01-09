19f573a3ca8368830ae9aff4285645419c349fa3
Chris <ulrich@tcs-germany.de>
2015-01-09 14:44:03 +0100

    AllObjects:fix delete when rights are set

--- promet/source/base/dbintfs/ubasedbclasses.pas
+++ promet/source/base/dbintfs/ubasedbclasses.pas
@@ -664,9 +664,9 @@ begin
       with Self.DataSet as IBaseDBFilter do
         begin
           if aId <> Null then
-            Filter := Data.QuoteField('SQL_ID')+'='+TBaseDBModule(DataModule).QuoteValue(Format('%d',[Int64(aID)]))
+            Filter := TBaseDBModule(DataModule).QuoteField(TableName)+'.'+Data.QuoteField('SQL_ID')+'='+TBaseDBModule(DataModule).QuoteValue(Format('%d',[Int64(aID)]))
           else
-            Filter := Data.QuoteField('SQL_ID')+'='+Data.QuoteValue('0');
+            Filter := TBaseDBModule(DataModule).QuoteField(TableName)+'.'+Data.QuoteField('SQL_ID')+'='+Data.QuoteValue('0');
           Limit := 0;
         end;
     end;
@@ -1103,20 +1103,23 @@ var
   aObj: TObjects;
 begin
   inherited Delete;
-  aObj := TObjects.Create(nil);
-  if not Data.TableExists(aObj.TableName) then
-    begin
-      aObj.CreateTable;
-      aObj.Free;
-      aObj := TObjects.CreateEx(nil,Data,nil,DataSet);
-    end;
-  aObj.SelectByRefId(Id.AsVariant);
-  aObj.Open;
-  while aObj.Count>0 do
-    begin
-      aObj.DataSet.Delete;
-    end;
-  aObj.Free;
+  try
+    aObj := TObjects.Create(nil);
+    if not Data.TableExists(aObj.TableName) then
+      begin
+        aObj.CreateTable;
+        aObj.Free;
+        aObj := TObjects.CreateEx(nil,Data,nil,DataSet);
+      end;
+    aObj.SelectByRefId(Id.AsVariant);
+    aObj.Open;
+    while aObj.Count>0 do
+      begin
+        aObj.DataSet.Delete;
+      end;
+    aObj.Free;
+  except
+  end;
 end;
 
 procedure TBaseDBDataset.Select(aID: Variant);
