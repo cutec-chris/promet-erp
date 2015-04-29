bd970720f378e1b9fd94b1c9576160a91cab0b33
Chris <ulrich@tcs-germany.de>
2015-04-29 12:48:51 +0200

    Add missing file

---
+++ promet/source/base/base_documents/promet_document_winthumbs.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit promet_document_winthumbs;
+
+interface
+
+uses
+  ShellObjHelper, ShObjIdlQuot, uwindowsshellpreviews, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('promet_document_winthumbs', @Register);
+end.
