2caf58ef99cc3d490ab75521116da6d5bb47eec5
Christian U <info@cu-tec.de>
2015-10-01 20:13:01 +0200

    Setup:trial to fix copy for win32

--- promet/setup/build-tools/setup_enviroment.sh
+++ promet/setup/build-tools/setup_enviroment.sh
@@ -5,23 +5,29 @@ if [ -e promet/source/base/version.inc ]
   then
   export lazbuild="lazbuild"
   export grep="grep"
-  export TARGET_CPU=$( lazbuild -? | grep 'powerpc_64' | sed 's/.*: \([^ ]\+\)$/\1/')
-  export TARGET_WIDGETSET=$( lazbuild -? | grep 'Carbon.' | sed 's/.*: \([^ ]\+\)$/\1/')
-  export TARGET_OS=$( lazbuild -? | grep 'linux.' | sed 's/.*: \([^ ]\+\)$/\1/')
+  export SED="sed"
+  $SED --version > /dev/null
+  if [ "$?" -ne "0" ]; then
+    export SED="$(PWD)/promet/setup/build-tools/sed.exe"
+  fi
+  export TARGET_CPU=$( lazbuild -? | grep 'powerpc_64' | $SED 's/.*: \([^ ]\+\)$/\1/')
+  export TARGET_WIDGETSET=$( lazbuild -? | grep 'Carbon.' | $SED 's/.*: \([^ ]\+\)$/\1/')
+  export TARGET_OS=$( lazbuild -? | grep 'linux.' | $SED 's/.*: \([^ ]\+\)$/\1/')
+  if [ "x$TARGET_OS" = "xwin32" ]; then
+    export TARGET_EXTENSION='.exe'
+    else
+    export TARGET_EXTENSION=''
+  fi
   if [ $TEMP ]
     then export BUILD_DIR=$TEMP/promet-build
     else export BUILD_DIR=/tmp/promet-build
-  fi
-  if [ $TARGET_OS="win32" ]
-    then
-    export TARGET_EXTENSION='.exe'
   fi
   Year=`date +%y`
   Month=`date +%m`
   Day=`date +%d`
   export BUILD_DATE=20$Year$Month$Day
-  Version=$(sed 's/\r//g' promet/source/base/version.inc).$(sed 's/\r//g' promet/source/base/revision.inc)
-  export BUILD_VERSION=$(echo $Version | sed 's/\n//g');
+  Version=$($SED 's/\r//g' promet/source/base/version.inc).$($SED 's/\r//g' promet/source/base/revision.inc)
+  export BUILD_VERSION=$(echo $Version | $SED 's/\n//g');
   echo "Version to Build:$BUILD_VERSION"
   export OUTPUT_DIR=promet/setup/output/$BUILD_VERSION
   # Set up widgetset
