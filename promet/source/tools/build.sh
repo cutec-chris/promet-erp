c0afbba40eb2a4d27e4e0a49f727115ae35cc8de
Christian U <info@cu-tec.de>
2015-10-18 13:43:38 +0200

    Setup:move cmdwozardmandant to extra build

--- promet/source/tools/build.sh
+++ promet/source/tools/build.sh
@@ -3,13 +3,6 @@ basedir=$(pwd)
 cd promet/source/tools
 . ../../setup/build-tools/setup_enviroment.sh
 echo "Building tools..."
-# Build components
-$lazbuild cmdwizardmandant.lpi $BUILD_ARCH $BUILD_PARAMS > build.txt
-if [ "$?" -ne "0" ]; then
-  echo "build failed"
-  $grep -w "Error:" build.txt
-  exit 1
-fi
 $lazbuild checkin.lpi $BUILD_ARCH $BUILD_PARAMS > build.txt
 if [ "$?" -ne "0" ]; then
   echo "build failed"
@@ -35,5 +28,5 @@ if [ "$?" -ne "0" ]; then
   exit 1
 fi
 cd $basedir/promet/output/$TARGET_CPU-$TARGET_OS
-zip $basedir/promet/setup/output/$BUILD_VERSION/tools_$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip cmdwizardmandant$TARGET_EXTENSION checkin$TARGET_EXTENSION checkout$TARGET_EXTENSION tools/processdaemon$TARGET_EXTENSION tools/processmanager$TARGET_EXTENSION
+zip $basedir/promet/setup/output/$BUILD_VERSION/tools_$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip checkin$TARGET_EXTENSION checkout$TARGET_EXTENSION tools/processdaemon$TARGET_EXTENSION tools/processmanager$TARGET_EXTENSION
 cd $basedir
