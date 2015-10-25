8e608dec709c34f97a60ca8a63922530e1fbfe12
Christian U <info@cu-tec.de>
2015-10-25 21:56:00 +0100

    Setup:upload help and importdata

--- promet/help/build.sh
+++ promet/help/build.sh
@@ -7,7 +7,7 @@ mkdir $BUILD_DIR
 cp help.db $BUILD_DIR
 sqlite3 $BUILD_DIR/help.db "delete from DOCUMENTS where TYPE<>'W';delete from HISTORY;delete from REPORTS;delete from ACCHISTORY;delete from TEMPLATES;delete from DOCPAGES;delete from DELETEDITEMS;delete from THUMBNAILS;delete from TASKS;delete from OPTIONS;delete from MESSAGEIDX;delete from MESSAGES;vacuum;"
 cd $BUILD_DIR
-target=help_$TARGET_CPU-$TARGET_OS
+target=help
 targetfile=$target-$BUILD_VERSION.zip
 targetcur=$target-current.zip
 zip -rq $basedir/promet/setup/output/$BUILD_VERSION/$targetfile .
--- promet/importdata/build.sh
+++ promet/importdata/build.sh
@@ -6,7 +6,7 @@ echo "Building importdata..."
 # Build components
 #set -xv
 #copy Files
-target=help_$TARGET_CPU-$TARGET_OS
+target=help
 targetfile=$target-$BUILD_VERSION.zip
 targetcur=$target-current.zip
 zip -rq $basedir/promet/setup/output/$BUILD_VERSION/$targetfile importdata
