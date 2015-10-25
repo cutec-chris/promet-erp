1eeef874fd4c2292dbe84ed7f8d16fe3c4a32851
Christian U <info@cu-tec.de>
2015-10-25 10:00:59 +0100

    Setup:dont abord when twitterreceiver is not build (needs newer fpc)

--- promet/source/sync/build_feed.sh
+++ promet/source/sync/build_feed.sh
@@ -14,7 +14,8 @@ $lazbuild twitterreceiver.lpi $BUILD_ARCH $BUILD_PARAMS > build.txt
 if [ "$?" -ne "0" ]; then
   echo "build failed"
   $grep -w "Error:" build.txt
-  exit 1
+#  twitterreceiver dont buildable with old fpc
+#  exit 1
 fi
 cd $basedir/promet/output/$TARGET_CPU-$TARGET_OS
 zip $basedir/promet/setup/output/$BUILD_VERSION/feedreceiver_$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip tools/feedreceiver$TARGET_EXTENSION
