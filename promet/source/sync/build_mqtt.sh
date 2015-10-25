2f887142905ede02987fbf2314bd34f2c5e196ed
Christian U <info@cu-tec.de>
2015-10-25 11:11:51 +0100

    Setup:fix Path for MQTT Receiver

--- promet/source/sync/build_mqtt.sh
+++ promet/source/sync/build_mqtt.sh
@@ -11,5 +11,5 @@ if [ "$?" -ne "0" ]; then
   exit 1
 fi
 cd $basedir/promet/output/$TARGET_CPU-$TARGET_OS
-zip $basedir/promet/setup/output/$BUILD_VERSION/mqttreceiver_$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip import_mqtt$TARGET_EXTENSION
+zip $basedir/promet/setup/output/$BUILD_VERSION/mqttreceiver_$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip tools/import_mqtt$TARGET_EXTENSION
 cd $basedir
