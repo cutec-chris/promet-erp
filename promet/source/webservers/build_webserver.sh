855ef481171b3a2c7a697d0bffd8d66e2f235693
Christian U <info@cu-tec.de>
2015-10-25 10:07:57 +0100

    Setup:build zips for webservers

--- promet/source/webservers/build_webserver.sh
+++ promet/source/webservers/build_webserver.sh
@@ -10,4 +10,6 @@ if [ "$?" -ne "0" ]; then
   $grep -w "Error:" build.txt
   exit 1
 fi
+cd $basedir/promet/output/$TARGET_CPU-$TARGET_OS
+zip $basedir/promet/setup/output/$BUILD_VERSION/webserver_$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip tools/webserver$TARGET_EXTENSION
 cd $basedir
