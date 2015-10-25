bb4a249b4659ef5f54e618abd4d337137f00e318
Christian U <info@cu-tec.de>
2015-10-25 17:20:45 +0100

    Setup:more work on upload

--- promet/setup/build-tools/doupload.sh
+++ promet/setup/build-tools/doupload.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scp -P 232 $1 autoupload@178.254.12.54:promet_upload_target
+scp -P 232 $basedir/promet/setup/output/$BUILD_VERSION/$1 autoupload@178.254.12.54:promet_upload_target
+ssh -P 232 autoupload@178.254.12.54 ln promet_upload_target/$2 promet_upload_target/$1
