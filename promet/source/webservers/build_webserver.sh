d8d507a3422e4313b46d4309a550345acddf2ffd
Christian U <info@cu-tec.de>
2015-10-25 11:30:07 +0100

    Setup:ignoring non compiling webserver

--- promet/source/webservers/build_webserver.sh
+++ promet/source/webservers/build_webserver.sh
@@ -8,7 +8,8 @@ $lazbuild webserver.lpi $BUILD_ARCH $BUILD_PARAMS > build.txt
 if [ "$?" -ne "0" ]; then
   echo "build failed"
   $grep -w "Error:" build.txt
-  exit 1
+#  exit 1
+#  build fails on old compilers
 fi
 cd $basedir/promet/output/$TARGET_CPU-$TARGET_OS
 zip $basedir/promet/setup/output/$BUILD_VERSION/webserver_$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip tools/webserver$TARGET_EXTENSION
