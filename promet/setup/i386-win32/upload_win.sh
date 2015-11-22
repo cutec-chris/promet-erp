9f6b548c7a7bf277378a3f68295342fbf065ec8e
Christian Ulrich <win@ullihome.de>
2015-11-22 13:53:18 +0100

    Setup:ability to upload WinSetupWizard

--- promet/setup/i386-win32/upload_win.sh
+++ promet/setup/i386-win32/upload_win.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-Version=$(sed 's/\r//g' ../../source/base/version.inc).$(sed 's/\r//g' ../../source/base/revision.inc)
-Version=$(echo $Version | sed 's/\n//g');
-chmod 644 output/*
-scp -P 232 ../output/*_$(echo $Version)_$1*.exe autoupload@178.254.12.54:promet_upload_target
-scp -P 232 ../output/db_setup*.exe autoupload@178.254.12.54:promet_upload_target
+scp -P 232 ../output/*.exe autoupload@178.254.12.54:promet_upload_target
