3d598bb6252c5ff8c282d39269d680cd652925ed
Christian U <info@cu-tec.de>
2016-04-15 22:12:04 +0200

    actual submodules

--- promet/source/components/lazbarcodes/packages/lazbarcodes.pas
+++ promet/source/components/lazbarcodes/packages/lazbarcodes.pas
@@ -4,6 +4,7 @@
 
 unit lazbarcodes;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
--- promet/source/projectmanagement/projectmanagement.pas
+++ promet/source/projectmanagement/projectmanagement.pas
@@ -4,6 +4,7 @@
 
 unit projectmanagement;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
