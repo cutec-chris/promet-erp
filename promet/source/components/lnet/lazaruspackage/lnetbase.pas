80b2ca50ff94d3beafe0049f11b4448c72a9bc13
Christian Ulrich <info@cu-tec.de>
2016-05-26 04:46:37 +0200

    actual submodules

--- promet/source/components/lnet/lazaruspackage/lnetbase.pas
+++ promet/source/components/lnet/lazaruspackage/lnetbase.pas
@@ -1,14 +1,14 @@
-{ Tento súbor bol automaticky vytvorený Lazarom. Prosím neurpavovať!
-  Tento zdrojový kód je použitý len na preloženie a nainštalovanie tohoto 
-    balíčka.
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
  }
 
-unit lnetbase; 
+unit lnetbase;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
-    lNet, lEvents, lCommon, lFTP, lsmtp, lTelnet, lhttp, lwebserver, 
+  lNet, lEvents, lCommon, lFTP, lsmtp, lTelnet, lhttp, lwebserver, 
   lMimeWrapper, lHTTPUtil, lControlStack, lMimeStreams, lMimeTypes, lProcess, 
   lSpawnFCGI, lStrBuffer, ltimer, lNetSSL, lThreadEvents, fastcgi_base;
 
