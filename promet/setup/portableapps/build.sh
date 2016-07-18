52b64e31452790eb7b651ff37681ba0e2b523f7e
Chris <ulrich@tcs-germany.de>
2016-07-18 14:04:38 +0200

    Portableapps:fix building

--- promet/setup/portableapps/build.sh
+++ promet/setup/portableapps/build.sh
@@ -20,25 +20,27 @@ unzip -u -d $BuildDir/App/promet $basedir/promet/setup/output/$BUILD_VERSION/vis
 rm $BuildDir/App/promet/helpviewer.exe
 unzip -u -d $BuildDir/App/promet $basedir/promet/setup/output/$BUILD_VERSION/mailreceiver_$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip
 echo "building package..."
-#cat Appinfo_devel.ini | \
-#  sed -b -e "s/VERSION/$Version/g" \
-#      -e "s/ARCH/$Arch/g" \
-#      -e "s/ARCHFPC/$Archfpc/g" \
-#      -e "s/CREATEDDATE/$Date/g" \
-#  > $BuildDir/App/AppInfo/appinfo.ini
-#rm $BuildDir/App/AppInfo/Launcher/Splash.jpg
+cat Appinfo_devel.ini | \
+  sed -b -e "s/VERSION/$Version/g" \
+      -e "s/ARCH/$Arch/g" \
+      -e "s/ARCHFPC/$Archfpc/g" \
+      -e "s/CREATEDDATE/$Date/g" \
+  > $BuildDir/App/AppInfo/appinfo.ini
+rm $BuildDir/App/AppInfo/Launcher/Splash.jpg
+/c/Windows/system32/cmd.exe "/C c:\PortableApps.comInstaller\PortableApps.comInstaller.exe %TEMP%\software_build
+"
 cat Appinfo.ini | \
   sed -b -e "s/VERSION/$Version/g" \
       -e "s/ARCH/$Arch/g" \
       -e "s/ARCHFPC/$Archfpc/g" \
       -e "s/CREATEDDATE/$Date/g" \
   > $BuildDir/App/AppInfo/appinfo.ini
-cp
-/c/PortableApps.comInstaller/PortableApps.comInstaller.exe $BuildDir
-cp $BuildDir/*.paf.exe ../output
+/c/Windows/system32/cmd.exe "/C c:\PortableApps.comInstaller\PortableApps.comInstaller.exe %TEMP%\software_build
+"
+cp $TmpDir/*.paf.exe ../output
 cd $BuildDir
-rm $basedir/../output/promet-erp-$(echo $Version).i386-win32-portable.zip
-zip -9 -r $basedir/../output/promet-erp-$(echo $Version).i386-win32-portable.zip Promet-ERP
+rm        $basedir/promet/setup/output/promet-erp-$Version.i386-win32-portable.zip
+zip -9 -r $basedir/promet/setup/output/promet-erp-$Version.i386-win32-portable.zip .
 echo "cleaning up..."
 
 cd $basedir
