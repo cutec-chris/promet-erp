545a140663d756e4d4977890a2c385c30430f6c1
Christian U <info@cu-tec.de>
2016-09-11 06:38:31 +0200

    Setup:add Script Plugins to Script Tools

---
+++ promet/source/scripts/plugins/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+basedir=$(pwd)
+cd promet/source/scripts/plugins
+. ../../setup/build-tools/setup_enviroment.sh
+echo "Building script plugins..."
+# Build components
+$lazbuild audio/audio.lpi $BUILD_ARCH $BUILD_PARAMS > build.txt
+$lazbuild dxfwrite/dxfwrite.lpi $BUILD_ARCH $BUILD_PARAMS >> build.txt
+$lazbuild hexfiles/hexfiles.lpi $BUILD_ARCH $BUILD_PARAMS >> build.txt
+$lazbuild net/net.lpi $BUILD_ARCH $BUILD_PARAMS >> build.txt
+$lazbuild serialport/serialport.lpi $BUILD_ARCH $BUILD_PARAMS >> build.txt
+$lazbuild tinkerforge/tinkerforge.lpi $BUILD_ARCH $BUILD_PARAMS >> build.txt
+$lazbuild usb/usb.lpi $BUILD_ARCH $BUILD_PARAMS >> build.txt
+$lazbuild video/video.lpi $BUILD_ARCH $BUILD_PARAMS >> build.txt
+
+cd $basedir/promet/output/$TARGET_CPU-$TARGET_OS
+#target=pscript_$TARGET_CPU-$TARGET_OS
+#targetfile=$target-$BUILD_VERSION.zip
+#targetcur=$target-current.zip
+#zip $basedir/promet/setup/output/$BUILD_VERSION/$targetfile pscript$TARGET_EXTENSION
+#if [ "$1" = "upload" ]; then
+#  . ../../setup/build-tools/doupload.sh $targetfile $targetcur
+#fi
+cd $basedir
