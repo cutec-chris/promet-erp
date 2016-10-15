777f75693fd74e7b49efaa626a39d5fa26d58865
Christian U <info@cu-tec.de>
2016-10-15 14:39:21 +0200

    Setup:Build Aplpha Wiki Page

--- promet/setup/build-tools/doupload.sh
+++ promet/setup/build-tools/doupload.sh
@@ -8,5 +8,10 @@ AUTOUPLOAD_HOST=downloads.free-erp.de
 
 echo "uploading $1..."
 scp -P $AUTOUPLOAD_PORT $basedir/promet/setup/output/$1 $AUTOUPLOAD_USER@$AUTOUPLOAD_HOST:promet_upload_target
-ssh $AUTOUPLOAD_USER@$AUTOUPLOAD_HOST -p $AUTOUPLOAD_PORT "cd promet_upload_target;ln -s -f $FILE $2"
+if [ "x$?" = "x0" ]
+  then
+  ssh $AUTOUPLOAD_USER@$AUTOUPLOAD_HOST -p $AUTOUPLOAD_PORT "cd promet_upload_target;ln -s -f $FILE $2"
+  echo "[downloads.free-erp.de/$1 $1]" >> act_alphadownload.txt
+  echo "Upload erfolgreich, Alpha Eintrag !"
+fi
 #scp $2 christian_u@frs.sourceforge.net:/home/frs/project/Promet-ERP/
--- promet/setup/upload_builds.sh
+++ promet/setup/upload_builds.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 . ./promet/setup/build-tools/setup_enviroment.sh
+echo "==Alpha Builds==" > ./promet/setup/output/act_alphadownload.txt
+echo "===Windows===" >> ./promet/setup/output/act_alphadownload.txt
 . ./promet/setup/i386-win32/upload.sh i386 win32
-. ./promet/setup/i386-linux/upload.sh i386 linux
+echo "===Linux x86_64 (64 bit)===" >> ./promet/setup/output/act_alphadownload.txt
 . ./promet/setup/i386-linux/upload.sh x86_64 linux
+echo "===Linux i386 (32 bit)===" >> ./promet/setup/output/act_alphadownload.txt
+. ./promet/setup/i386-linux/upload.sh i386 linux
+echo "===Linux arm (z.b. Raspberry pi)===" >> ./promet/setup/output/act_alphadownload.txt
 . ./promet/setup/i386-linux/upload.sh arm linux
