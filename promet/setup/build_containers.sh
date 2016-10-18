edf5c1e64f179af63706fb74c6f4fc21409ce58b
Christian U <info@cu-tec.de>
2016-10-18 05:24:57 +0200

    Setup:next trial to update Container

--- promet/setup/build_containers.sh
+++ promet/setup/build_containers.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 . ./promet/setup/build-tools/setup_enviroment.sh
 cd /tmp
-git clone http://gitlab-ci-token:$CI_BUILD_TOKEN@192.168.177.120:10080/promet/promet-client-docker.git
+git clone https://gitlab-ci-token:$CI_BUILD_TOKEN@192.168.177.120:10080/promet/promet-client-docker.git
 cd promet-client-docker
-cat ./Dockerfile.template | \
-  sed -e "s/VERSION/$BUILD_VERSION/g"
-  > Dockerfile
+cat ./Dockerfile.template | sed -e "s/VERSION/$BUILD_VERSION/g" > ./Dockerfile
 git commit -a -m "Dockerfile with Version updated"
 git push origin master
