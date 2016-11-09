b0073cea46a6986e8af2297c49ab057ea6517c79
Christian U <info@cu-tec.de>
2016-11-09 21:28:14 +0100

    Trial to upload alpha wiki Page

--- promet/setup/change_wiki_alpha.sh
+++ promet/setup/change_wiki_alpha.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 basedir=$(pwd)
 cd promet/setup
-Version=$(sed 's/\r//g' ../../source/base/version.inc).$(sed 's/\r//g' ../../source/base/revision.inc)
+Version=$(sed 's/\r//g' ../source/base/version.inc).$(sed 's/\r//g' ../source/base/revision.inc)
 Version=$(echo $Version | sed 's/\n//g');
 Arch=`dpkg --print-architecture`
 Archfpc=$(fpc -h | grep 'Compiler version' | sed 's/.*for \([^ ]\+\)$/\1/')
 Date=`date`
+echo $STORA_CONN > ~/.prometerp/Stora.perml
 
 lazbuild ../source/tools/changewikipage.lpi
 echo Promet-ERP/dowloadplattforms/linux-$Archfpc
