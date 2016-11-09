b5f1d02ac7587afee0bec8f58c2ff8616feb777d
Christian U <info@cu-tec.de>
2016-11-09 19:32:22 +0100

    Setup:deploy alpha Wiki

---
+++ promet/setup/change_wiki_alpha.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+basedir=$(pwd)
+cd promet/setup
+Version=$(sed 's/\r//g' ../../source/base/version.inc).$(sed 's/\r//g' ../../source/base/revision.inc)
+Version=$(echo $Version | sed 's/\n//g');
+Arch=`dpkg --print-architecture`
+Archfpc=$(fpc -h | grep 'Compiler version' | sed 's/.*for \([^ ]\+\)$/\1/')
+Date=`date`
+
+lazbuild ../source/tools/changewikipage.lpi
+echo Promet-ERP/dowloadplattforms/linux-$Archfpc
+../output/$Archfpc-linux/changewikipage --mandant=Stora Promet-ERP/dowloadplattforms/alpha ../output/act_alphadownload.txt
+../output/$Archfpc-linux/changewikipage --mandant=Stora Promet-ERP/changes ../source/base/changes.txt
+../output/$Archfpc-linux/sync_db --mandant=Stora --db=Help
+../output/$Archfpc-linux/sync_db --mandant=Stora --db=Server1
+cd $basedir
