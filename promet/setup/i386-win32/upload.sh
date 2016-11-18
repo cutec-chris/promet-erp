771732cdf8d3fe7347b668e8b9d7c9642cce3315
Christian U <info@cu-tec.de>
2016-11-18 20:33:11 +0100

    Setup:more Downloads in Nigthly Builds

--- promet/setup/i386-win32/upload.sh
+++ promet/setup/i386-win32/upload.sh
@@ -5,6 +5,49 @@ cd promet/setup/i386-win32
 echo "Uploading win32 stuff..."
 TARGET_CPU=$1
 TARGET_OS=$2
+cd $basedir
+echo "====Portable Installation====" >> ./promet/setup/output/act_alphadownload.txt
+cd $basedir/promet/setup/output
+for f in *.paf.exe
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  cd promet/setup/i386-win32
+  targetfile=$f
+  targetcur=$target-current.paf.exe
+  cd $basedir/promet/setup/output
+  . ../../setup/build-tools/doupload.sh $targetfile $targetcur
+  cd ..
+  cd $basedir
+  done
+
+cd $basedir
+echo "====Zip Dateien====" >> ./promet/setup/output/act_alphadownload.txt
+echo "Diese Downloads werden normalerweise intern vom Windows Setup oder Updatern intern verwendet. Sie können jedoch auch verwendet werden um sich eine maßgeschneiderte Installation zusammenzubauen. Sie können einfach in ein Verzeichnis entpackt werden und von dort gestartet." >> ./promet/setup/output/act_alphadownload.txt
+cd promet/setup/i386-win32
+target=win32tools_$TARGET_CPU-$TARGET_OS
+targetfile=$target-$BUILD_VERSION.zip
+targetcur=$target-current.zip
+cd $basedir/promet/setup/output
+. ../../setup/build-tools/doupload.sh $targetfile $targetcur
+cd ..
+
+cd $basedir
+for f in *$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  cd promet/setup/i386-win32
+  targetfile=$f
+  targetcur=$target-current.zip
+  cd $basedir/promet/setup/output
+  . ../../setup/build-tools/doupload.sh $targetfile $targetcur
+  cd ..
+  cd $basedir
+  done
+cd $basedir
+
+echo "====Bibliotheken====" >> ./promet/setup/output/act_alphadownload.txt
 # Build components
 target=sqliteclient_$TARGET_CPU-$TARGET_OS
 targetfile=$target-$BUILD_VERSION.zip
@@ -29,30 +72,5 @@ targetcur=$target-current.zip
 cd $basedir/promet/setup/output
 . ../../setup/build-tools/doupload.sh $targetfile $targetcur
 cd ..
-cd $basedir
-echo "====Zip Dateien====" >> ./promet/setup/output/act_alphadownload.txt
-echo "Diese Downloads werden normalerweise intern vom Windows Setup oder Updatern intern verwendet. Sie können jedoch auch verwendet werden um sich eine maßgeschneiderte Installation zusammenzubauen Sie können einfach in ein Verzeichnis entpackt werden und von dort gestartet." >> ./promet/setup/output/act_alphadownload.txt
-cd promet/setup/i386-win32
-target=win32tools_$TARGET_CPU-$TARGET_OS
-targetfile=$target-$BUILD_VERSION.zip
-targetcur=$target-current.zip
-cd $basedir/promet/setup/output
-. ../../setup/build-tools/doupload.sh $targetfile $targetcur
-cd ..
-
-cd $basedir
-for f in *$TARGET_CPU-$TARGET_OS-$BUILD_VERSION.zip
-do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  cd promet/setup/i386-win32
-  targetfile=$f
-  targetcur=$target-current.zip
-  cd $basedir/promet/setup/output
-  . ../../setup/build-tools/doupload.sh $targetfile $targetcur
-  cd ..
-  cd $basedir
-  done
-cd $basedir
 
 
