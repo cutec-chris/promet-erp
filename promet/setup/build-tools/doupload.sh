587e1679efa8eb71c23af07e856f60fc3393238f
Christian U <info@cu-tec.de>
2016-11-19 11:17:04 +0100

    Setup:fix Links on Alpha Page

--- promet/setup/build-tools/doupload.sh
+++ promet/setup/build-tools/doupload.sh
@@ -11,7 +11,7 @@ scp -P $AUTOUPLOAD_PORT $basedir/promet/setup/output/$1 $AUTOUPLOAD_USER@$AUTOUP
 if [ "x$?" = "x0" ]
   then
   ssh $AUTOUPLOAD_USER@$AUTOUPLOAD_HOST -p $AUTOUPLOAD_PORT "cd promet_upload_target;ln -s -f $FILE $2"
-  echo "[downloads.free-erp.de/$1 $1]" >> act_alphadownload.txt
+  echo "[http://downloads.free-erp.de/$1 $1]" >> act_alphadownload.txt
   echo "Upload erfolgreich, Alpha Eintrag !"
 fi
 #scp $2 christian_u@frs.sourceforge.net:/home/frs/project/Promet-ERP/
