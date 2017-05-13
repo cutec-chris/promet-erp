92ae21169ebfddaef4f56b340737a15d4b249909
Christian Ulrich <info@cu-tec.de>
2017-05-13 09:14:35 +0200

    Setup:publish also alpine Server container

--- promet/setup/build_containers.sh
+++ promet/setup/build_containers.sh
@@ -93,3 +93,33 @@ JSON
 echo $PAYLOAD$Content'"}]}' > Dockerfile.tmp
 
 curl -vv --request POST --header "PRIVATE-TOKEN: $API_BUILD_TOKEN" --header "Content-Type: application/json" --data @Dockerfile.tmp http://192.168.177.120:10080/api/v3/projects/159/repository/commits
+
+rm Dockerfile*
+wget http://192.168.177.120:10080/promet/promet-server-alpine-docker/raw/master/Dockerfile.template
+
+Content=$(cat ./Dockerfile.template | sed -e "s/PVERSION/$BUILD_VERSION/g" | sed -e "s/RANDOMNUMBER/$RANDOM/g")
+Content=${Content//\\/\\\\} # \
+#Content=${Content//\//\\\/} # /
+Content=${Content//\'/\\\'} # ' (not strictly needed ?)
+Content=${Content//\"/\\\"} # "
+Content=${Content//   /\\t} # \t (tab)
+Content=${Content//
+/\\\n} # \n (newline)
+Content=${Content//^M/\\\r} # \r (carriage return)
+Content=${Content//^L/\\\f} # \f (form feed)
+Content=${Content//^H/\\\b} # \b (backspace)
+
+PAYLOAD=$(cat << 'JSON'
+{
+  "branch_name": "master",
+  "commit_message": "Patch Version in Dockerfile",
+  "actions": [
+    {
+      "action": "update",
+      "file_path": "Dockerfile",
+      "content": "
+JSON
+)
+echo $PAYLOAD$Content'"}]}' > Dockerfile.tmp
+
+curl -vv --request POST --header "PRIVATE-TOKEN: $API_BUILD_TOKEN" --header "Content-Type: application/json" --data @Dockerfile.tmp http://192.168.177.120:10080/api/v3/projects/160/repository/commits
