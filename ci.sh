d6e29d217203768a099cb04273d328e3427c5ce5
chris <christian@ullihome.de>
2017-06-30 07:33:37 +0000

    'ci.sh' ändern

--- ci.sh
+++ ci.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-docker run -it --name=ci_build_64 buildhost-lazarus-x86_64
+docker run --name=ci_build_64 build-lazarus-x64
 docker exec ci_build_64 git clone --depth=1 http://192.168.177.120:10090/promet/promet-erp.git
 docker exec ci_build_64 cd promet-erp
 docker exec ci_build_64 git submodule sync --recursive
 docker exec ci_build_64 git submodule update --init --recursive
+
+docker kill ci_build_64
+docker rm ci_build_64
