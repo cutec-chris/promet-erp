af500811c62fbb133a88ce2e383090415c732b81
chris <christian@ullihome.de>
2017-06-30 08:01:20 +0000

    'build.sh' ändern

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@
 # importdata - Zip the standard Importdata
 # all - compiling all
 # default - compiling program only (using by default)
-cd $(pwd)
+cd "$0"
 ls
 . ./promet/setup/build-tools/setup_enviroment.sh
 
