5ec45732b6eeae27b11126d101356d5ae8c19817
chris <christian@ullihome.de>
2017-06-30 12:25:39 +0000

    'ci.sh' ändern

--- ci.sh
+++ ci.sh
@@ -4,4 +4,4 @@ docker run --rm -v /home/chris/gogs/jenkins/workspace/Promet-ERP:/root build-laz
 docker run --rm -v /home/chris/gogs/jenkins/workspace/Promet-ERP:/root build-lazarus-i386 bash /root/build.sh
 #armhf
 #sudo apt-get install qemu-user-static
-docker run --rm -v /home/chris/gogs/jenkins/workspace/Promet-ERP:/root -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static  build-lazarus-armhf bash /root/build.sh
+docker run --rm -v /home/chris/gogs/jenkins/workspace/Promet-ERP:/root -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static  build-lazarus-armhf bash /root/build.sh server
