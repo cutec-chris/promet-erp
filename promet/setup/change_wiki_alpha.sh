5d663159f9daec4bbaf60644eb0d58728b62b181
chris <christian@ullihome.de>
2017-07-06 18:41:01 +0000

    'promet/setup/change_wiki_alpha.sh' ändern

--- promet/setup/change_wiki_alpha.sh
+++ promet/setup/change_wiki_alpha.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 basedir=$(pwd)
-cd promet/setup
+cd $(dirname "$0")
 Version=$(sed 's/\r//g' ../source/base/version.inc).$(sed 's/\r//g' ../source/base/revision.inc)
 Version=$(echo $Version | sed 's/\n//g');
 Arch=`dpkg --print-architecture`
